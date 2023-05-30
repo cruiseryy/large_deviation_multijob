781265f77317678a626255fcf01afcb247bd0f6c
cruiseryy <xp53@cornell.edu>
2023-05-30 14:22:45 +0800

    change path

--- mv_ic.py
+++ mv_ic.py
@@ -2,9 +2,9 @@ import os
 
 # '/data/projects/11002298/long/xpwrf/1981/wrfbdy_d01'
 source = '/data/projects/11002298/long/xpwrf/'
-dest = '/home/users/nus/xp53/scratch/lds_test/wrf_demo/'
+dest = '/scratch/users/nus/xp53/lds_multijob/large_deviation_multijob/'
 
-for yy in range(1981, 1990):
+for yy in range(1981, 2021):
     source2 = source + str(yy) + '/wrfrst_d01_' + str(yy-1) + '-12-01_00:00:00'
     dest2 = dest + 'wrf_ic/wrfrst_d01_' + str(yy-1) + '-12-01_00:00:00'
     cmd2 = 'cp -f ' + source2 + ' ' + dest2
