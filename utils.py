6295f0ce2762d021b43a2a77038af07b0232e329
cruiseryy <xp53@cornell.edu>
2023-06-05 11:36:39 +0800

    new rain data retriever

--- utils.py
+++ utils.py
@@ -1,85 +1,84 @@
 import numpy as np
 import xarray as xr
+from datetime import datetime, timedelta
 # from matplotlib import pyplot as plt
 # import cartopy.crs as ccrs
 
 class ReadRainfall:
-    def __init__(self, n = 32, path = '') -> None:
+    def __init__(self, n = 32, path = '00', T = 5) -> None:
         self.n = n 
-        self.path = path 
-        self.ic = np.loadtxt(self.path + 'ic.txt').astype(int)
-        # self.weights = np.loadtxt(self.path + 'weights.txt')
-        self.parents = np.loadtxt(self.path + 'topo.txt').astype(int)
+        if path == '00':
+            self.path = '/scratch/users/nus/xp53/lds_multijob/large_deviation_multijob/vars/path.txt'
+        else:
+            self.path = ''
+        self.ic = np.loadtxt(self.path + '/vars/ic.txt').astype(int)
+        self.bc_record = np.loadtxt(self.path + '/vars/bc_record.txt').astype(int)
+        self.parent = np.loadtxt(self.path + '/vars/topo.txt').astype(int)
+        self.R = np.loadtxt(self.path + '/vars/R.txt')
+        self.weights = np.loadtxt(self.path + '/vars/weights.txt')
+        self.T = T
         pause = 1
         return
+    
+    def get_ymd(self, i, icy):
+        start = str(icy) + '-12-01'
+        start_date = datetime.strptime(start, '%Y-%m-%d')
+        end_date = start_date + timedelta(days=i)
+        return end_date.strftime('%Y-%m-%d')
     
     def read_data(self):
         tmp_pre = 'wrfrst_d01_'
         tmp_suf = '_00:00:00'
-        self.traj = np.zeros([self.n, 7])
+        
         for j in range(self.n):
-            tmp_rain = np.zeros([7, 120, 160])
-            ii = 8
-            cur = self.path + 'traj/' + '{:02d}'.format(j) + '/' + tmp_pre + str(self.ic[j, 1]-1) + '-12-' + '{:02d}'.format(ii) + tmp_suf
+            rain = np.zeros([self.T*6, 120*160])
+            mark = 0
+            for t in range(self.T):
+                for i in range(6):
+                    tmpymd = self.get_ymd(i = t*5 + i, icy = self.ic[j, t]-1)
+                    tmpfile = self.path + 'traj/' + '{:02d}'.format(j) + '/' + tmp_pre + tmpymd + tmp_suf
+                    tmp_rain = xr.open_dataset(tmpfile)['RAINNC'][0,:,:]
+                    tmp_rain_flat = tmp_rain.reshape(tmp_rain.shape[0], -1)
+                    rain[mark, :] = tmp_rain_flat
+                    mark += 1
             
-            ii -= 1
-            while ii >=  1:
-                if ii >= 6:
-                    nex = self.path + 'traj/' + '{:02d}'.format(j) + '/' + tmp_pre + str(self.ic[j, 1]-1) + '-12-' + '{:02d}'.format(ii) + tmp_suf
-                    self.traj[j, ii-1] = self.ic[j, 1]
-                else:
-                    nj = self.parents[j,1]
-                    nex = self.path + 'traj/' + '{:02d}'.format(nj) + '/' + tmp_pre + str(self.ic[nj, 0]-1) + '-12-' + '{:02d}'.format(ii) + tmp_suf
-                    self.traj[j, ii-1] = self.ic[nj, 0]
-                tmp_rain[ii-1, :, :] = xr.open_dataset(cur)['RAINNC'][0,:,:] - xr.open_dataset(nex)['RAINNC'][0,:,:]
-                cur = nex 
-                ii -= 1
-            rain_flat = tmp_rain.reshape(tmp_rain.shape[0], -1)
+            np.savetxt(self.path + '/output/rainfall_' + str(j) +'.txt', rain)
+
+            # rain_flat = tmp_rain.reshape(tmp_rain.shape[0], -1)
             # data_flat = np.loadtxt(dest)
             # data = data_flat.reshape(data_flat.shape[0], 120, 160)
-            np.savetxt(self.path + 'traj/rainfall_' + str(j) +'.txt', rain_flat)
-            pause = 1
-        # fig = plt.figure()
-        # ax = plt.axes(projection=ccrs.PlateCarree())
-        # ax.coastlines(resolution='10m')
-        # cmap = plt.get_cmap('Blues', 20)
-        # tmp = xr.open_dataset(cur)
-        # pmap = plt.scatter(tmp['XLONG'][0], tmp['XLAT'][0], c = tmp_rain[0,:,:], cmap=cmap)
-        # cbar = plt.colorbar(pmap)
-        # cbar.set_label('Annual Prcp (mm)')
             
         return
     
-    def read_data2(self):
-        tmp_pre = 'wrfrst_d01_'
-        tmp_suf = '_00:00:00'
+    # def read_data2(self):
+    #     tmp_pre = 'wrfrst_d01_'
+    #     tmp_suf = '_00:00:00'
 
-        for j in range(self.n):
+    #     for j in range(self.n):
 
-            tmp_path = self.path + 'traj/' + '{:02d}'.format(j) + '/' + tmp_pre 
+    #         tmp_path = self.path + 'traj/' + '{:02d}'.format(j) + '/' + tmp_pre 
 
-            tmp_rain = np.zeros([9, 120, 160])
-            ii = [1, 2, 3, 4, 5, 6, 6, 7, 8]
+    #         tmp_rain = np.zeros([9, 120, 160])
+    #         ii = [1, 2, 3, 4, 5, 6, 6, 7, 8]
 
-            for i in range(9):
+    #         for i in range(9):
 
-                idx = ii[i]
+    #             idx = ii[i]
 
-                if i < 6: 
-                    tmp_file = tmp_path + str(self.ic[j, 0]-1) + '-12-' + '{:02d}'.format(idx) + tmp_suf
-                else:
-                    tmp_file = tmp_path + str(self.ic[j, 1]-1) + '-12-' + '{:02d}'.format(idx) + tmp_suf
+    #             if i < 6: 
+    #                 tmp_file = tmp_path + str(self.ic[j, 0]-1) + '-12-' + '{:02d}'.format(idx) + tmp_suf
+    #             else:
+    #                 tmp_file = tmp_path + str(self.ic[j, 1]-1) + '-12-' + '{:02d}'.format(idx) + tmp_suf
 
-                tmp_rain[i, :, :] = xr.open_dataset(tmp_file)['RAINNC'][0,:,:]
+    #             tmp_rain[i, :, :] = xr.open_dataset(tmp_file)['RAINNC'][0,:,:]
 
 
-            rain_flat = tmp_rain.reshape(tmp_rain.shape[0], -1)
-            np.savetxt(self.path + 'traj/rainfall_' + str(j) +'.txt', rain_flat)
+    #         rain_flat = tmp_rain.reshape(tmp_rain.shape[0], -1)
+    #         np.savetxt(self.path + 'traj/rainfall_' + str(j) +'.txt', rain_flat)
 
-        return
+    #     return
     
 
 if __name__ == "__main__":
-    test = ReadRainfall()
-    # test.read_data()
-    test.read_data2()
+    test = ReadRainfall(n = 128)
+    test.read_data()
